d3697e2299b7f8914ddc28b85ebda038c262684a
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-09-28 23:14:50 +0530

    setting up routes

---
+++ app/(auth)/sign-in/Page.tsx
@@ -0,0 +1,9 @@
+
+
+const SignIn = () => {
+    return (
+        <div>Page</div>
+    )
+}
+
+export default SignIn
---
+++ app/(auth)/sign-up/Page.tsx
@@ -0,0 +1,9 @@
+
+
+const SignUp = () => {
+    return (
+        <div>Page</div>
+    )
+}
+
+export default SignUp
